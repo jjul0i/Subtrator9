8ab6121b8e7839f8226be3710d749ca111ed4bf4
jjul0i <josesilva47@estudante.ufscar.br>
2026-07-03 22:36:23 -0300

    Update top.v

--- top.v
+++ top.v
@@ -1,12 +1,43 @@
-
 module top (
     input  [31:0] a,
     input  [31:0] b,
-    input         sub,         // 0 = soma, 1 = subtração
+    input         sub,
     output [31:0] result,
     output        carry_out,
-    output        overflow);
+    output        overflow
+);
+
+wire [31:0] bx;
+wire [32:0] c;
+
+assign bx = b ^ {32{sub}};
+assign c[0] = sub;
+
+genvar i;
+generate
+    for (i = 0; i < 32; i = i + 1) begin : fa
+        full_adder U (
+            .a(a[i]),
+            .b(bx[i]),
+            .cin(c[i]),
+            .s(result[i]),
+            .cout(c[i+1])
+        );
+    end
+endgenerate
+
+assign carry_out = c[32];
+assign overflow = c[31] ^ c[32];
+
+endmodule
 
 
+module full_adder (
+    input  a, b, cin,
+    output s, cout
+);
+
+assign s = a ^ b ^ cin;
+assign cout = (a & b) | (cin & (a ^ b));
 
 endmodule
